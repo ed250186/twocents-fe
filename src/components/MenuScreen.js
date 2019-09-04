68b44134fa20ddabbee8a59d29ab9f6040d32ca4
Nathan <nathan.froeh@gmail.com>
2019-09-04 17:01:36 -0600

    Make MenuSpacing variable

--- src/components/MenuScreen.js
+++ src/components/MenuScreen.js
@@ -11,11 +11,13 @@ export class MenuScreen extends Component {
             style={{ width: 300, height: 300 }}
           />
         </View>
-        <Text style={styles.text}>TwoCents</Text>
-        <Text style={styles.about}>TwoCents is a cross pollination student project presented in Module 4 of Turing School of Software and Design</Text>
-        <Text style={styles.links}>Add Recommendation</Text>
-        <Text style={styles.links}>Notifications</Text>
-        <Text style={styles.links}>Contact</Text>
+        <View style={styles.textBox}>
+          <Text style={styles.text}>TwoCents</Text>
+          <Text style={styles.about}>TwoCents is a cross pollination student project presented in Module 4 of Turing School of Software and Design</Text>
+          <Text style={styles.links}>Add Recommendation</Text>
+          <Text style={styles.links}>Notifications</Text>
+          <Text style={styles.links}>Contact</Text>
+        </View>
         <View style={styles.yelp}>
           <Text style={styles.yelpText}>Powered by</Text>
           <Image source={require("../images/Yelp_trademark_RGB_outline.png")} style={{ width: 100, height: 40, marginTop: -15 }}/>
@@ -29,29 +31,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#2C2540",
-    marginBottom: 0
+    marginBottom: 0,
+    alignItems: 'center',
   },
   image: {
-    alignItems: 'center'
+    alignItems: 'center',
+    height: '35%',
+    width: '100%',
+  },
+  textBox: {
+    height: '60%',
+    width: '90%',
   },
   text: {
     color: "#EE933F",
     fontSize: 50,
     textAlign: "center",
-    marginTop: -20,
   },
   about: {
     color: '#CCC0DD',
     fontSize: 24,
     textAlign: 'justify',
-    padding: 20,
+    paddingTop: 20,
+    paddingBottom: 50,
     lineHeight: 0
   },
   links: {
     color: "#EE933F",
     fontSize: 32,
     textAlign: 'left',
-    padding: 20,
+    paddingTop: 20,
+    paddingBottom: 20,
     lineHeight: 10
   },
   yelpText: {
