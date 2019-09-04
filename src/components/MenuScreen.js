77a842bcc79b0b0c38bc25deb687a4fe587a60c7
Emily Dittmer <emilyedittmer@gmail.com>
2019-09-03 19:26:11 -0600

    Created Menu page

--- src/components/MenuScreen.js
+++ src/components/MenuScreen.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
 
-export class MenuScreen extends React.Component {
+export class MenuScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Menu</Text>
+        <View style={styles.image}>
+          <Image
+            source={require("../images/twocents-logo.png")}
+            style={{ width: 300, height: 300 }}
+          />
+        </View>
+        <Text style={styles.text}>TwoCents</Text>
+        <Text style={styles.about}>TwoCents is a cross pollination student project presented in Module 4 of Turing School of Software and Design</Text>
+        <View style={styles.allLinks}>
+          <Text style={styles.links}>Add Recommendation</Text>
+          <Text style={styles.links}>Notifications</Text>
+          <Text style={styles.links}>Contact</Text>
+        </View>
       </View>
     );
   }
@@ -14,12 +26,30 @@ export class MenuScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2C2540',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#2C2540",
+    marginBottom: 0
+  },
+  image: {
+    alignItems: 'center'
   },
   text: {
-    color: '#EE933F',
-    fontSize: 30,
+    color: "#EE933F",
+    fontSize: 50,
+    textAlign: "center",
+    marginTop: -20,
+  },
+  about: {
+    color: '#CCC0DD',
+    fontSize: 24,
+    textAlign: 'justify',
+    padding: 20,
+    lineHeight: 0
+  },
+  links: {
+    color: "#EE933F",
+    fontSize: 32,
+    textAlign: 'left',
+    padding: 20,
+    lineHeight: 10
   }
 });
