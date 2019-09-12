2529120425228a8e03e60200644f02f881282ad5
Emily Dittmer <emilyedittmer@gmail.com>
2019-09-11 21:16:54 -0600

    All errorReducers tested

--- src/reducers/errorReducer.test.js
+++ src/reducers/errorReducer.test.js
@@ -1,7 +1,31 @@
-import errorReducer from './errorReducer'
+import { errorReducer } from './errorReducer'
 
-describe('ErrorReducer', () => {
+describe('errorReducer', () => {
   it('should return the initial state', () => {
-    
+    const expected = '';
+    const result = errorReducer(undefined, '')
+    expect(result).toEqual(expected) 
+  })
+
+  it('should set an error to state', () => {
+    const action = {
+      type: 'HAS_ERRORED',
+      message: 'Error'
+    }
+
+    const expected = 'Error'
+    const result = errorReducer(undefined, action);
+    expect(result).toEqual(expected)
+  })
+
+  it('should clear the error message', () => {
+    const action = {
+      type: 'CLEAR_ERROR',
+      message: ''
+    }
+
+    const expected = ''
+    const result = errorReducer(undefined, action);
+    expect(result).toEqual(expected)
   })
 })
