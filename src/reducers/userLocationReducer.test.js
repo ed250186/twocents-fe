5136975286ca188112d74bc80c9cfa8630834e4a
Emily Dittmer <emilyedittmer@gmail.com>
2019-09-11 21:44:47 -0600

    Test initial state in userLocationReducer

--- src/reducers/userLocationReducer.test.js
+++ src/reducers/userLocationReducer.test.js
@@ -1,2 +1,9 @@
 import { userLocationReducer } from './userLocationReducer';
 
+describe('userLocationReducer', () => {
+  it('should return the initial state', () => {
+    const expected = [];
+    const result = userLocationReducer(undefined, [])
+    expect(result).toEqual(expected) 
+  })
+})
