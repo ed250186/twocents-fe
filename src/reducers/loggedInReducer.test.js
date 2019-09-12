75629dac03402a540480d74923dc858d42d5d8a9
Emily Dittmer <emilyedittmer@gmail.com>
2019-09-11 21:25:05 -0600

    Finish loggedInReducer tests

--- src/reducers/loggedInReducer.test.js
+++ src/reducers/loggedInReducer.test.js
@@ -6,4 +6,27 @@ describe('loggedInReducer', () => {
     const result = loggedInReducer(undefined, {})
     expect(result).toEqual(expected) 
   })
+
+  it('should log in a user', () => {
+    const action = {
+      type: 'IS_LOGGED_IN',
+      user: {
+        isLoggedIn: true,
+        name: 'Emily',
+        email: 'emily@email.com',
+        ImageUrl: 'picture',
+        key: 12345
+      }
+    }
+
+    const expected = {
+      isLoggedIn: true,
+      name: 'Emily',
+      email: 'emily@email.com',
+      ImageUrl: 'picture',
+      key: 12345
+    }
+    const result = loggedInReducer(undefined, action);
+    expect(result).toEqual(expected)
+  })
 })
