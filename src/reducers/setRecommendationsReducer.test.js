5c1eaa7f92e0745c910dae009e3a8532c1552a40
Emily Dittmer <emilyedittmer@gmail.com>
2019-09-11 21:40:33 -0600

    Set recommendation test added

--- src/reducers/setRecommendationsReducer.test.js
+++ src/reducers/setRecommendationsReducer.test.js
@@ -6,4 +6,19 @@ describe('setRecommendationsReducer', () => {
     const result = setRecommendationsReducer(undefined, [])
     expect(result).toEqual(expected) 
   })
+
+  it('should get all the recommendations', () => {
+    const action = {
+      type: 'RECOMMENDATIONS',
+      payload:[
+        {name: 'Little Owl'}
+      ]
+    }
+
+    const expected = [{
+      name: 'Little Owl'
+    }]
+    const result = setRecommendationsReducer(undefined, action);
+    expect(result).toEqual(expected)
+  })
 })
