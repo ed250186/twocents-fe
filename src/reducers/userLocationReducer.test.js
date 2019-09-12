b1b4fe405bad4019bdc9abd29791210c2379ed55
Emily Dittmer <emilyedittmer@gmail.com>
2019-09-11 21:49:14 -0600

    Finish userLocation testing

--- src/reducers/userLocationReducer.test.js
+++ src/reducers/userLocationReducer.test.js
@@ -6,4 +6,20 @@ describe('userLocationReducer', () => {
     const result = userLocationReducer(undefined, [])
     expect(result).toEqual(expected) 
   })
+  it('should return the user location', () => {
+    const action = {
+      type: 'GET_USER_LOCATION',
+      location:[
+        {latitude: 1},
+        {longitude: 2}
+      ]
+    }
+
+    const expected = [
+      {latitude: 1},
+      {longitude: 2}
+    ]
+    const result = userLocationReducer(undefined, action);
+    expect(result).toEqual(expected)
+  })
 })
