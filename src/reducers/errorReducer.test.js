09cfce457039942a641cb88dc7287ba6b8b4b0fb
Emily Dittmer <emilyedittmer@gmail.com>
2019-09-11 21:08:09 -0600

    start errorReducer tests

--- src/reducers/errorReducer.test.js
+++ src/reducers/errorReducer.test.js
@@ -1,21 +1,7 @@
 import errorReducer from './errorReducer'
 
 describe('ErrorReducer', () => {
-  it('Should return default state', () => {
-    const mockState = undefined;
-    const expected = '';
-    const result = ErrorReducer(mockState, {})
-
-    expect(result).toEqual(expected)
-  })
-  it('Should return error message when action type is HAS ERRORED', () => {
-    const mockAction = {
-      type: 'HAS ERRORED',
-      error: 'Mock error message'
-    }
-    const expected = 'Mock error message'
-    const result = ErrorReducer('', mockAction)
+  it('should return the initial state', () => {
     
-    expect(result).toEqual(expected)
   })
 })
